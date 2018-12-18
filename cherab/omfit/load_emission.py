3e2ec60d0b4d3518c5bc1b987628c3d781d09e21
shenders <stuart.henderson@ukaea.uk>
2018-12-18 15:11:32 +0000

    Update loading of emission

--- cherab/omfit/load_emission.py
+++ cherab/omfit/load_emission.py
@@ -4,7 +4,8 @@ from cherab.core.atomic.elements import hydrogen, deuterium, carbon, helium, nit
 from cherab.core.model import ExcitationLine, RecombinationLine
 
 from cherab.openadas import OpenADAS
-
+from cherab.openadas.install import install_adf15
+from cherab.openadas.repository import add_wavelength
 
 _SPECIES_LOOKUP = {
     "hydrogen": hydrogen,
@@ -29,17 +30,32 @@ def load_emission(config, plasma):
     models = []
 
     plasma.atomic_data = OpenADAS(permit_extrapolation=True)
-
+    if config["plasma"]["installADF15"]:
+        for adf15 in config["plasma"]["adf15"]:
+            try:
+                species = _SPECIES_LOOKUP[adf15["species"]]
+            except KeyError:
+                raise ValueError("The species emission specification for this config file is invalid.")                                   
+            charge     = adf15["ionisation"]
+            file_path  = adf15["file_path"]
+            adas_path  = adf15["adas_path"]
+            install_adf15(species,charge,file_path,adas_path=adas_path)
+            
+    
     for emission_instruction in config["plasma"]["emission_instructions"]:
-
+       
         try:
             species = _SPECIES_LOOKUP[emission_instruction["species"]]
         except KeyError:
-            raise ValueError("The species emission specification for this config file is invalid.")
-
+            raise ValueError("The species emission specification for this config file is invalid.")                                   
         ionisation = emission_instruction["ionisation"]
-        upper = emission_instruction["upper"]
-        lower = emission_instruction["lower"]
+        upper      = emission_instruction["upper"]
+        lower      = emission_instruction["lower"]
+        transition =(upper,lower)
+        wavelength = emission_instruction["wavelength"]
+        if not wavelength == 0: 
+            print ("Adding wavelength")
+            add_wavelength(species,ionisation,transition,wavelength)
 
         # Do a test call to see if the species exists on this plasma.
         _ = plasma.composition.get(species, ionisation)

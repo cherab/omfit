072c1cb7794c55785a17caa5f340737852f8394a
shenders <stuart.henderson@ukaea.uk>
2019-10-23 14:48:42 +0100

    Fix array indexing

--- cherab/omfit/load_dms.py
+++ cherab/omfit/load_dms.py
@@ -21,11 +21,12 @@ def load_dms_output(config,world,plasma,spec,fibgeom):
         fib = np.linspace(1,fibgeom.numfibres,num=fibgeom.numfibres)
         
     for i, f in enumerate(fib):
+        arridx = int(f)-1
         print ("Analysing fibre: ",int(f))
         fibgeom.set_fibre(number=int(f))        
         start_point    = Point3D(fibgeom.origin[0],fibgeom.origin[1],fibgeom.origin[2])
         forward_vector = Vector3D(fibgeom.xhat(),fibgeom.yhat(),fibgeom.zhat()).normalise()
-        up_vector      = Vector3D(0, 0, 1.0)
+        up_vector      = Vector3D(1.0, 1.0, 1.0)
         if config['dms']['power_pipeline']:
             power          = PowerPipeline0D()
             fibre  = FibreOptic([spectra], acceptance_angle=config['dms']['acceptance_angle'], radius=config['dms']['radius'], spectral_bins=spec.pixels, spectral_rays=config['dms']['spectral_rays'],
@@ -33,9 +34,8 @@ def load_dms_output(config,world,plasma,spec,fibgeom):
             fibre.min_wavelength = spec.wlower
             fibre.max_wavelength = spec.wupper
             fibre.observe()
-            power_arr[f] = power.value.mean
-        else:
-            power_arr[f] = None
+            power_arr[arridx] = power.value.mean
+
         if config['dms']['radiance_pipeline']:
             spectra = SpectralRadiancePipeline0D(display_progress=False)
             fibre  = FibreOptic([spectra], acceptance_angle=config['dms']['acceptance_angle'], radius=config['dms']['radius'], spectral_bins=spec.pixels, spectral_rays=config['dms']['spectral_rays'],
@@ -43,9 +43,8 @@ def load_dms_output(config,world,plasma,spec,fibgeom):
             fibre.min_wavelength = spec.wlower
             fibre.max_wavelength = spec.wupper
             fibre.observe()
-            spectra_arr[:,f] = spectra.samples.mean
-        else:
-            spectra_arr[:,f] = None
+            spectra_arr[:,arridx] = spectra.samples.mean
+
         if config['dms']['los_profiles']:
             n0 = plasma.composition.get(nitrogen, 0)
             n1 = plasma.composition.get(nitrogen, 1)
@@ -60,22 +59,17 @@ def load_dms_output(config,world,plasma,spec,fibgeom):
                 x = start_point.x + fibgeom.xhat() * t
                 y = start_point.y + fibgeom.yhat() * t
                 z = start_point.z + fibgeom.zhat() * t
-                te_los[j,f] = plasma.electron_distribution.effective_temperature(x,y,z)
-                ne_los[j,f] = plasma.electron_distribution.density(x,y,z)
-                nN_los[j,f,0] = n0.distribution.density(x, y, z)
-                nN_los[j,f,1] = n1.distribution.density(x, y, z)
-                nN_los[j,f,2] = n2.distribution.density(x, y, z)
-                nN_los[j,f,3] = n3.distribution.density(x, y, z)
-                nN_los[j,f,4] = n4.distribution.density(x, y, z)
-                nN_los[j,f,5] = n5.distribution.density(x, y, z)
-                nN_los[j,f,6] = n6.distribution.density(x, y, z)
-                nN_los[j,f,7] = n7.distribution.density(x, y, z)
-                d_los[j,f]  = t
-        else:
-            d_los[:,f]  = None
-            te_los[:,f] = None
-            ne_los[:,f] = None
-            nN_los[:,f,:] = None
+                te_los[j,arridx] = plasma.electron_distribution.effective_temperature(x,y,z)
+                ne_los[j,arridx] = plasma.electron_distribution.density(x,y,z)
+                nN_los[j,arridx,0] = n0.distribution.density(x, y, z)
+                nN_los[j,arridx,1] = n1.distribution.density(x, y, z)
+                nN_los[j,arridx,2] = n2.distribution.density(x, y, z)
+                nN_los[j,arridx,3] = n3.distribution.density(x, y, z)
+                nN_los[j,arridx,4] = n4.distribution.density(x, y, z)
+                nN_los[j,arridx,5] = n5.distribution.density(x, y, z)
+                nN_los[j,arridx,6] = n6.distribution.density(x, y, z)
+                nN_los[j,arridx,7] = n7.distribution.density(x, y, z)
+                d_los[j,arridx]  = t
 
     return power_arr,spectra_arr,te_los,ne_los,nN_los,d_los
 

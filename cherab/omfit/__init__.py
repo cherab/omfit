fe3409478cf82d973849e143e4bd10a9d746d7f8
shenders <stuart.henderson@ukaea.uk>
2019-10-24 13:00:01 +0100

    Add better structure for retrieving 2D profiles

--- cherab/omfit/__init__.py
+++ cherab/omfit/__init__.py
@@ -2,5 +2,6 @@
 from .load_machine import load_machine
 from .load_plasma import load_edge_simulation
 from .load_emission import load_emission
+from .load_profiles import load_profiles
 from .load_camera import load_camera
 from .load_dms import load_dms_output, load_dms_fibres, load_dms_spectrometer
--- cherab/omfit/load_emission.py
+++ cherab/omfit/load_emission.py
@@ -27,10 +27,11 @@ _EMISSION_TYPE_LOOKUP = {
 }
 
 
-def load_emission(config, plasma, n_density, emiss2d, xr, yr):
+def load_emission(config, plasma, xr, yr, num):
 
-    models   = []
-    tmpmodel = []
+    models    = []
+    tmpmodel  = []
+    em_plasma = np.zeros((num,num))
     plasma.atomic_data = OpenADAS(permit_extrapolation=True)
     if config["plasma"]["installADF15"]:
         for adf15 in config["plasma"]["adf15"]:
@@ -74,7 +75,7 @@ def load_emission(config, plasma, n_density, emiss2d, xr, yr):
         else:
             models.append(_EMISSION_TYPE_LOOKUP[emission_instruction["type"]](line))
 
-        if emission_instruction["prof2D"]:
+        if emission_instruction["prof2D"] and config['plasma']['edge']['em2D']:
             line_detail = _EMISSION_TYPE_LOOKUP[emission_instruction["type"]](line)
             tmpmodel.append(line_detail)
             plasma.models = tmpmodel
@@ -82,15 +83,8 @@ def load_emission(config, plasma, n_density, emiss2d, xr, yr):
             wlgth = data.wavelength(species,ionisation,transition)
             for i, x in enumerate(xr):
                 for j, y in enumerate(yr):
-                    emiss2d[j,i] = emiss2d[j,i] + line_detail.emission(Point3D(x, 0.0, y), Vector3D(1.0, 0, 0),
-                                                                        Spectrum(wlgth-5.0, wlgth+5.0, 100)).total()
-
+                    em_plasma[j,i] = em_plasma[j,i] + line_detail.emission(Point3D(x, 0.0, y), Vector3D(1.0, 0, 0),
+                                                                           Spectrum(wlgth-5.0, wlgth+5.0, 100)).total()
     plasma.models = models
 
-    n1 = plasma.composition.get(nitrogen, 1)
-    n2 = plasma.composition.get(nitrogen, 2)
-
-    for i, x in enumerate(xr):
-        for j, y in enumerate(yr):
-            n_density[j, i, 1] = n1.distribution.density(x, 0.0, y)
-            n_density[j, i, 2] = n2.distribution.density(x, 0.0, y)
+    return em_plasma
